e8eef17dd6d333987b43edd527c1448c394347f9
MrlnM <Muehle-m@web.de>
2022-07-17 15:08:48 +0200

    Update profession.cs

--- profession.cs
+++ profession.cs
@@ -5,18 +5,30 @@ class Profession
     public int TotalNumber = 0;
 
     //Konstruktor
+<<<<<<< Updated upstream
     Profession(int total)
+=======
+    public Profession(int total)
+>>>>>>> Stashed changes
     {
         AgeGroups=null;
         TotalNumber = total;
     }
 
+<<<<<<< Updated upstream
     Profession (List<AgeGroup> ages) {
+=======
+    public Profession (List<AgeGroup> ages) {
+>>>>>>> Stashed changes
         AgeGroups = ages;
         foreach (AgeGroup age in AgeGroups) TotalNumber += age.Number;
     }
 
+<<<<<<< Updated upstream
     Profession (List<AgeGroup> ages, int total) {
+=======
+    public Profession (List<AgeGroup> ages, int total) {
+>>>>>>> Stashed changes
         AgeGroups = ages;
         TotalNumber = total;
     }
