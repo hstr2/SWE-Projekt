5e6f4d3ada87b87487a3425b8f16da6970506ac7
hstr2 <103415381+hstr2@users.noreply.github.com>
2022-07-19 16:08:45 +0200

    Add files via upload

---
+++ table.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Data;
+using System.Data.OleDb;
+
+class Table
+{
+    public void ADONET_dynTabelle1()
+    {
+
+    DataTable Table = new DataTable();
+    //Spalten erzeugen
+    Table.Columns.Add("year", System.Type.GetType(System.Int32));
+    Table.Columns.Add("students", System.Type.GetType(System.Int32));
+    Table.Columns.Add("teacher", System.Type.GetType(System.Int32));
+    Table.Columns.Add("needed teacher", System.Type.GetType(System.Int32));
+    Table.Columns.Add("schools", System.Type.GetType(System.Int32));
+    Table.Columns.Add("needed schools", System.Type.GetType(System.Int32));
+
+    //1. Zeile erzeugen
+    DataRow row = Table.NewRow();
+    //Wert fehlt
+    row ["year"];
+    row ["students"];
+    row ["teacher"];
+    row ["needed teacher"];
+    row ["schools"];
+    row ["needed schools"];
+    //Anfügen an Tabelle
+    Table.Row.Add(row);
+    //weitere sind identisch
+    }
+}
