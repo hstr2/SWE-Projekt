33c6b096ef30f3f39ab73352241b1af7ee04b341
MrlnM <104758862+MrlnM@users.noreply.github.com>
2022-07-26 08:59:01 +0200

    Revert "Output"

--- table.cs
+++ table.cs
@@ -4,7 +4,7 @@ using System.Data.OleDb;
 
 class Table
 {
-    public void ADONET_dynTabelle1(Calculation calc)
+    public void ADONET_dynTabelle1()
     {
         
     DataTable Table = new DataTable();
@@ -20,11 +20,11 @@ class Table
     DataRow row = Table.NewRow();
     //Wert fehlt
     row ["year"] = +5;
-    row ["students"] = calc.students5;
-    row ["teacher"] = calc.teachers5;
-    row ["needed teacher"] = calc.teachersneeded5;
-    row ["schools"] = calc.schools;
-    row ["needed schools"] = calc.schoolsneeded5;
+    row ["students"] = students5;
+    row ["teacher"] = teachers5;
+    row ["needed teacher"] = teachersneeded5;
+    row ["schools"] = schools;
+    row ["needed schools"] = schoolsneeded5;
     //Anfügen an Tabelle
     Table.Rows.Add(row);
     //weitere sind identisch
@@ -32,33 +32,33 @@ class Table
     DataRow row = Table.NewRow();
     //Wert fehlt
     row ["year"] = +10;
-    row ["students"] = calc.students10;
-    row ["teacher"] = calc.teachers10;
-    row ["needed teacher"] = calc.teachersneeded10;
-    row ["schools"] = calc.schools;
-    row ["needed schools"] = calc.schoolsneeded10;
+    row ["students"] = students10;
+    row ["teacher"] = teachers10;
+    row ["needed teacher"] = teachersneeded10;
+    row ["schools"] = schools;
+    row ["needed schools"] = schoolsneeded10;
     //Anfügen an Tabelle
     Table.Rows.Add(row);
     //3.Zeile
     DataRow row = Table.NewRow();
     //Wert fehlt
     row ["year"] = +15;
-    row ["students"] = calc.students15;
-    row ["teacher"] = calc.teachers15;
-    row ["needed teacher"] = calc.teachersneeded15;
-    row ["schools"] = calc.schools;
-    row ["needed schools"] = calc.schoolsneeded15;
+    row ["students"] = students15;
+    row ["teacher"] = teachers15;
+    row ["needed teacher"] = teachersneeded15;
+    row ["schools"] = schools;
+    row ["needed schools"] = schoolsneeded15;
     //Anfügen an Tabelle
     Table.Rows.Add(row);
     //4.Zeile
     DataRow row = Table.NewRow();
     //Wert fehlt
     row ["year"] = +20;
-    row ["students"] = calc.students20;
-    row ["teacher"] = calc.teachers20;
-    row ["needed teacher"] = calc.teachersneeded20;
-    row ["schools"] = calc.schools;
-    row ["needed schools"] = calc.schoolsneeded20;
+    row ["students"] = students20;
+    row ["teacher"] = teachers20;
+    row ["needed teacher"] = teachersneeded20;
+    row ["schools"] = schools;
+    row ["needed schools"] = schoolsneeded20;
     //Anfügen an Tabelle
     Table.Rows.Add(row);
     }
